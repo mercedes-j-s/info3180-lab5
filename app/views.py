e422884e40f84c719bd5b9b40565ca0ea500345b
Mercedes Smith <mercedes.smith@mymona.uwi.edu>
2022-03-02 20:05:22 -0500

    Login Route updated

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -34,7 +34,8 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        # if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
@@ -42,12 +43,22 @@ def login():
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            username = form.username.data
+            password = form.password.data
 
-            # get user id, load into session
-            login_user(user)
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            if user is not None and check_password_hash(user.password, password):
+                # get user id, load into session
+                login_user(user)
+
+                # remember to flash a message to the user
+                flash('Logged in successfully', 'success')
+
+                next_page = request.args.get('next')
+                return redirect(next_page or url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect', 'danger')
     return render_template("login.html", form=form)
 
 
